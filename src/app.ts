5ad776a06ef1d5799a034e3700608a425689267b
Jack Caulfield <42452695+jackcauliflower@users.noreply.github.com>
2023-05-30 14:15:35 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 try {
   console.log('Started refreshing application (/) commands.');
 
-  await rest.patch(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
+  await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands });
 
   console.log('Successfully reloaded application (/) commands.');
 } catch (error) {
