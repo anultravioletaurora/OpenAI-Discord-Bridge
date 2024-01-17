4347007fc0fbfad2d8d208011fb841cf264fb960
Violet Caulfield <violet@cosmonautical.cloud>
2024-01-17 12:05:43 -0600

    Comment out old not used stuff for now

--- src/commands/chat.ts
+++ src/commands/chat.ts
@@ -1,34 +1,34 @@
-import { SlashCommandBuilder, SlashCommandStringOption } from 'discord.js';
-import { Configuration, OpenAIApi } from "openai";
+// import { SlashCommandBuilder, SlashCommandStringOption } from 'discord.js';
+// import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
 
-const data = new SlashCommandBuilder()
-  .setName("chat")
-  .setDescription("Sends a message to ChatGPT")
-  .addStringOption(new SlashCommandStringOption()
-    .setName("prompt")
-    .setDescription("What do you want ChatGPT to do?")
-    .setRequired(true))
-  .setDMPermission(false)
-  .setDefaultMemberPermissions(2);
+// const data = new SlashCommandBuilder()
+//   .setName("chat")
+//   .setDescription("Sends a message to ChatGPT")
+//   .addStringOption(new SlashCommandStringOption()
+//     .setName("prompt")
+//     .setDescription("What do you want ChatGPT to do?")
+//     .setRequired(true))
+//   .setDMPermission(false)
+//   .setDefaultMemberPermissions(2);
 
-async function execute(interaction) {
-  // trigger a deferred response, otherwise the 3-second timeout will kill this request
-  await interaction.deferReply();
+// async function execute(interaction) {
+//   // trigger a deferred response, otherwise the 3-second timeout will kill this request
+//   await interaction.deferReply();
 
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { role: "system", content: "You are a sassy and sarcastic assistant" },
-      { role: "user", content: interaction.options.getString("prompt") }
-    ],
-  });
+//   const response = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [
+//       { role: "system", content: "You are a sassy and sarcastic assistant" },
+//       { role: "user", content: interaction.options.getString("prompt") }
+//     ],
+//   });
 
-  await interaction.followUp(response.data.choices[0].message);
-}
+//   await interaction.followUp(response.data.choices[0].message);
+// }
 
-export { data, execute };
+// export { data, execute };
