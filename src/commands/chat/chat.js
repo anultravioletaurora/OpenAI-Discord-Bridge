18a4b927def3838a3b36183344444347f4b9c077
Violet Caulfield <violet@cosmonautical.cloud>
2024-01-17 14:53:06 -0600

    Adding logging

--- src/commands/chat/chat.js
+++ src/commands/chat/chat.js
@@ -20,6 +20,8 @@ module.exports = {
   async execute(interaction) {
       // trigger a deferred response, otherwise the 3-second timeout will kill this request
       await interaction.deferReply();
+
+      console.debug(`Discord interaction options: ${interaction.options}`)
     
       const response = await openai.chat.completions.create({
         model: process.env.MODEL ?? "gpt-3.5-turbo",
