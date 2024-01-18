35bcf8f5ec526698f9d2d5664efccc814b073456
Violet Caulfield <violet@cosmonautical.cloud>
2024-01-17 22:48:17 -0600

    Got it working again!

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
+const { REST, Routes } = require('discord.js');
 
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
-function registerCommands() {
+function registerCommands(client) {
+	const commands = [];
+	// Grab all the command folders from the commands directory you created earlier
 	const foldersPath = path.join(__dirname, 'commands');
-
 	const commandFolders = fs.readdirSync(foldersPath);
 
 	for (const folder of commandFolders) {
+		// Grab all the command files from the commands directory you created earlier
 		const commandsPath = path.join(foldersPath, folder);
 		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 		for (const file of commandFiles) {
 			const filePath = path.join(commandsPath, file);
 			const command = require(filePath);
-			// Set a new item in the Collection with the key as the command name and the value as the exported module
 			if ('data' in command && 'execute' in command) {
+				commands.push(command.data.toJSON());
 				client.commands.set(command.data.name, command);
 			} else {
 				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 			}
 		}
 	}
+
+	// Construct and prepare an instance of the REST module
+	const rest = new REST().setToken(process.env.BOT_TOKEN);
+
+	// and deploy your commands!
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+			// The put method is used to fully refresh all commands in the guild with the current set
+			const data = await rest.put(
+				Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+				{ body: commands },
+			);
+
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		} catch (error) {
+			// And of course, make sure you catch and log any errors!
+			console.error(error);
+		}
+	})();
 }
 
 // Create a new client instance
@@ -36,7 +61,7 @@ const client = new Client({
 
 client.commands = new Collection();
 
-registerCommands();
+registerCommands(client);
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
